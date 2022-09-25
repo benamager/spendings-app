67a2a32e8b43176a8bddfd060c807faf802f5a30
benamager <benaprivat@gmail.com>
2022-09-25 17:20:59 +0200

    Played around with data storage locally

--- App.tsx
+++ App.tsx
@@ -1,21 +1,51 @@
 /* Imports */
 import { NavigationContainer } from "@react-navigation/native"
 import BottomTabNavigator from "./src/routes/BottomTabNavigator"
+import { useEffect, useState } from "react"
+import AsyncStorage from "@react-native-async-storage/async-storage"
 
 /* Main app */
 export default function App() {
+  const date: any = new Date()
+  //console.log(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate())
+
+  const setStringValue = async (value: any) => {
+    try {
+      await AsyncStorage.setItem("key", value)
+    } catch (e) {
+      // save error
+    }
+
+    console.log("Done.")
+  }
+  setStringValue("Hello World")
+
+  const getMyStringValue = async () => {
+    try {
+      return await AsyncStorage.getItem("key")
+    } catch (e) {
+      // read error
+    }
+
+    console.log("Done.")
+  }
+  console.log(getMyStringValue())
+
+  // const [data, setData] = useState({
+  //   "2022": {
+  //     "9": {
+  //       "24": {
+  //         title: "My Birthday",
+  //         description: "I'm turning 18",
+  //         category: "mad",
+  //       },
+  //     },
+  //   },
+  // })
+
   return (
     <NavigationContainer>
       <BottomTabNavigator />
     </NavigationContainer>
   )
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-// })
