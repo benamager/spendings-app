edb6a25bc6fde1495097cd39cae0cf764e6eac62
benamager <benaprivat@gmail.com>
2022-10-19 21:10:41 +0200

    Played around with table component

--- src/screens/Stats/index.tsx
+++ src/screens/Stats/index.tsx
@@ -1,5 +1,5 @@
 /* stats.tsx screen */
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import screenWrapper from "../../styles/screenWrapper";
 import PieChart from "../../components/PieChart";
@@ -10,10 +10,29 @@ import SpentThisMonth from "../../components/SpentThisMonth";
 import StatsUpDown from "../../components/StatsUpDown";
 import style from "./style";
 
+// for table
+import {
+  Table,
+  TableWrapper,
+  Row,
+  Rows,
+  Col,
+} from "react-native-table-component";
+
 // Stats page
 const Stats = () => {
   const [month, setMonth] = useState("2022-10");
   const [activeTag, setActiveTag] = useState("Uge");
+
+  // for table
+  const tableHead = ["", "Kategori", "Procent", "Brugt"];
+  const tableTitle = ["🍔", "💩", "😉", "💪🏼"];
+  const tableData = [
+    ["Kantine mad", "2", "360"],
+    ["Dumme penge", "b", "5000"],
+    ["Smarte ting", "2", "100"],
+    ["Træning", "b", "31"],
+  ];
 
   return (
     <View style={screenWrapper.style}>
@@ -50,9 +69,42 @@ const Stats = () => {
           setActiveTag={setActiveTag}
         />
       </View>
+      <View style={styles.container}>
+        <Table borderStyle={{ borderWidth: 0 }}>
+          <Row
+            data={tableHead}
+            flexArr={[1, 3, 2, 2]}
+            style={styles.head}
+            textStyle={styles.text}
+          />
+          <TableWrapper style={styles.wrapper}>
+            <Col
+              data={tableTitle}
+              style={styles.title}
+              heightArr={[28, 28]}
+              textStyle={styles.text}
+            />
+            <Rows
+              data={tableData}
+              flexArr={[2, 1, 1]}
+              style={styles.row}
+              textStyle={styles.text}
+            />
+          </TableWrapper>
+        </Table>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: { flex: 1, paddingTop: 40, backgroundColor: "#fff" },
+  head: { height: 30, backgroundColor: "#f1f8ff" },
+  wrapper: { flexDirection: "row" },
+  title: { flex: 1, backgroundColor: "#f6f8fa" },
+  row: { height: 28 },
+  text: { textAlign: "center" },
+});
 
 export default Stats;
